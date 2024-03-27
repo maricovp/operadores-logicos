ae8d96258884b4fd0b5dce21206c9e26dabcfe7e
maricovp <marcos2015vinicuis@gmail.com>
2024-03-27 10:59:31 -0400

    questao21 adicionada

---
+++ questao21.js
@@ -0,0 +1,4 @@
+/*Explique como você pode usar operadores lógicos para verificar se uma 
+pessoa tem idade entre 18 e 65 anos em JavaScrip
+ utilizando let  para ver se é maior que 18, e utilizando o var pra complementar me dizendo se é maior que 65 ent é false 
+ 18> true <65> true */
