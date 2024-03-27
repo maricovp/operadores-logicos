90c6b159776b274d69611f169fa9ba2b35c29071
maricovp <marcos2015vinicuis@gmail.com>
2024-03-27 10:58:51 -0400

    questao15 adicionada

---
+++ questao15.js
@@ -0,0 +1,3 @@
+/*Explique como a ordem de avaliação dos operadores lógicos afeta o 
+resultado de uma expressão em JavaScript.
+a ordem de avaliação deve ser no inicio  por conta que alterara o resultado se colocar depois*/
