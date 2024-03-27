f820e3f4b712307485fbdc24d3fced5e93ee7910
maricovp <marcos2015vinicuis@gmail.com>
2024-03-27 11:00:54 -0400

    questao30 adicionada

---
+++ questao30.js
@@ -0,0 +1,3 @@
+/*Explique como você pode usar operadores lógicos para verificar se um 
+número é negativo e maior que -10 em JavaScript*/
+/*utilizando o operadores de comparação quando o numero for  >-10  true e quando for < -10 false */
